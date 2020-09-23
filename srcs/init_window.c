cd4907c2781fc781e65911ce418630a0e22e6230
Alexandre Pelissier <alpeliss@mi-r6-p3.s19.be>
2020-09-23 08:45:39 +0200

    ca passe?

--- srcs/init_window.c
+++ srcs/init_window.c
@@ -11,38 +11,12 @@
 /* ************************************************************************** */
 
 #include "mini_rt.h"
-/*
-static void		print_point(t_point point, char *name)
-{
-	printf("%s: ", name);
-	printf("x = %f, y = %f, z = %f\n", point.x, point.y, point.z);
-}
-*/
+
 void		print_vector(t_vector r)
 {
 	printf("origin x = %f, y = %f, z= %f\ndir x = %f, y = %f, z = %f\n\n"
 	, r.origin.x, r.origin.y, r.origin.z, r.dir.x, r.dir.y, r.dir.z);
 }
-
-/*float	inter(t_vector r, t_env *e)
-{
-	float		res;
-	t_shapes	*tmp;	
-
-	tmp = e->s;
-	res = 0;
-	while (tmp)
-	{
-	//	printf("A");
-		if (e->s->id == 0)
-			res += inter_sphere(r, tmp, *e);
-		tmp = tmp->next;
-	}
-//	printf("\n\n");
-//	res = (res * 10000 > 1) ? 1 : res * 10000;
-	return (res);
-}
-*/
 
 double		calc_color(double intens, t_env *e, t_shapes *s)
 {
@@ -57,55 +31,46 @@ double		calc_color(double intens, t_env *e, t_shapes *s)
 	best_intens.y = (best_intens.y < 0) ? 0 : best_intens.y;
 	best_intens.z = (intens != -1) ? intens + e->li_ambi * e->li_color.z : 0;
 	best_intens.z = (best_intens.z > 255) ? 255 : best_intens.z;
-	best_intens.z = (best_intens.z < 0) ? 0 : best_intens.z;		
-    color = (int)(best_intens.z * s->color.z);
-    color += (int)(best_intens.y * s->color.y) * 256;
-    color += (int)(best_intens.x * s->color.x) * 256 * 256;
+	best_intens.z = (best_intens.z < 0) ? 0 : best_intens.z;
+	color = (int)(best_intens.z * s->color.z);
+	color += (int)(best_intens.y * s->color.y) * 256;
+	color += (int)(best_intens.x * s->color.x) * 256 * 256;
 	return (color);
 }
 
 double	inter_sphere(t_vector v, t_shapes *s)
 {
+	double	b;
+	double	c;
+	double	t1;
+	double	t2;
+	double	delta;
 
-    double  	b;
-    double  	c;
-    double  	t1;
-    double  	t2;
-    double  	delta;
-
-    //resolution interection sphere rayon
-    b = 2 * scal_prod(v.dir, basic_op_point(v.origin, s->pos, 0));
-    c = square_norm(basic_op_point(v.origin, s->pos, 0)) - s->diameter * s->diameter;
-    delta = b * b - 4 * c;
-    if (delta < 0)
-        return -1;
-    t1 = (-b - sqrt(delta)) / 2;
-    t2 = (-b + sqrt(delta)) / 2;
-
-    //la plus proche inter
-    t1 = (t1 > 0) ? t1 : t2;
-    if (t2 < 0) 
-        return -1;
-	//if (*t != -1)
-	//	printf("t1 = %f && *t = %f\n", t1, *t);
-	//if  (t1 > *t && *t != -1)
-	//	return -1;
+	b = 2 * scal_prod(v.dir, basic_op_point(v.origin, s->pos, 0));
+	c = square_norm(basic_op_point(v.origin, s->pos, 0))
+		- s->diameter * s->diameter;
+	delta = b * b - 4 * c;
+	if (delta < 0)
+		return (-1);
+	t1 = (-b - sqrt(delta)) / 2;
+	t2 = (-b + sqrt(delta)) / 2;
+	t1 = (t1 > 0) ? t1 : t2;
+	if (t2 < 0)
+		return (-1);
 	return (t1);
 }
 
-double     inter(t_vector v, t_env *e)
+double		inter(t_vector v, t_env *e)
 {
 	t_shapes	*s;
 	t_shapes	*c;
-    double  	t;
-    double  	intensite;
-    t_point 	pos;
-    t_point 	normale;
-	int			test;
+	double		t;
+	double		intensite;
+	t_point		pos;
+	t_point		normale;
 
 	s = e->s;
 	t = -1;
-	test = 0;
 	while (s)
 	{
 		if (s->id == 0)
@@ -117,54 +82,16 @@ double     inter(t_vector v, t_env *e)
 				c = s;
 			}
 		}
-		if (s->diameter == 20 && t != -1)
-		{
-			test = 1;
-			printf("t = %f", t);
-		}
 		s = s->next;
 	}
-	if (test)
-		printf("t = %f\n", t);
 	if (t == -1)
 		return (0);
-
-    // coordonnée de l'inter 
-    pos = basic_op_point(v.origin, mult_point(v.dir, t, 1), 1);
-    
-    // normal à l'inter
-    normale = normalize(basic_op_point(pos, e->s->pos, 0));
-
-    //calcul intensité lumière
-    intensite = scal_prod(normalize(basic_op_point(e->l->pos, pos, 0)), normale) * e->l->ratio * 1000000;
-    intensite /= square_norm(basic_op_point(e->l->pos, pos, 0));
+	pos = basic_op_point(v.origin, mult_point(v.dir, t, 1), 1);
+	normale = normalize(basic_op_point(pos, e->s->pos, 0));
+	intensite = scal_prod(normalize(basic_op_point(e->l->pos, pos, 0)), normale) * e->l->ratio * 1000000;
+	intensite /= square_norm(basic_op_point(e->l->pos, pos, 0));
 	return (calc_color(intensite, e, c));
 }
-
-/*void	init_tab(t_env *e)
-{
-	unsigned long	i;
-	unsigned long	j;
-	t_vector	r;
-
-	r.dir.z = -(float)e->res_x / (2 * tan((e->c->fov * PI) / 360));
-	r.origin.x = e->c->pos.x;
-	r.origin.y = e->c->pos.y;
-	r.origin.z = e->c->pos.z;
-	i = 0;
-	while (i < e->res_y)
-	{
-		j = 0;
-		while (j < e->res_x)
-		{
-			r.dir.x = (float)j - (float)e->res_x / 2;
-			r.dir.y = (float)i - (float)e->res_y / 2;	
-			e->mlx->tab[i * e->res_x + j] = (inter(r, e) * 255);
-			j++;
-		}
-		i++;
-	}
-}*/
 
 int			mini_check(t_env *e)
 {
@@ -175,28 +102,25 @@ int			mini_check(t_env *e)
 
 void		init_tab(t_env *e)
 {
-    int		i;
-    int		j;
-    t_vector    	v;
+	int			i;
+	int			j;
+	t_vector	v;
 
 	v.origin = e->c->pos;
 	i = 0;
-    while (i < e->res_y)
-    {
-        j = 0;
-        while (j < e->res_x)
-        {
-            v.dir.x = (long)j - e->res_x / 2; //W / 2
-           	v.dir.y = (long)i - e->res_y / 2; //H / 2   
-            v.dir.z = -((float)e->res_x / 2) / tan((e->c->fov * PI) / 360); // -W / (2 *tan(alpha / 2))
-			v.dir = normalize(v.dir);	
+	while (i < e->res_y)
+	{
+		j = 0;
+		while (j < e->res_x)
+		{
+			v.dir.x = (long)j - e->res_x / 2;
+			v.dir.y = (long)i - e->res_y / 2;
+			v.dir.z = -((float)e->res_x / 2) / tan((e->c->fov * PI) / 360);
+			v.dir = normalize(v.dir);
 			e->mlx->tab[(e->res_y - i - 1) * e->res_x + j] = inter(v, e);
-            //e->mlx->tab[(e->res_y - i - 1) * e->res_x + j] = (int)(intensite.z * e->s->color.z);
-            //e->mlx->tab[(e->res_y - i - 1) * e->res_x + j] += (int)(intensite.y * e->s->color.y) * 256;
-            //e->mlx->tab[(e->res_y - i - 1) * e->res_x + j] += (int)(intensite.x * e->s->color.x) * 256 * 256;
-            j++;
-        }
-        i++;
+			j++;
+		}
+		i++;
 	}
 }
 
